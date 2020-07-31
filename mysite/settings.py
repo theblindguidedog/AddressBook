2b782f12cd0c89f6710f3a8c2b2063eab179ea29
Horace The Blind Guide Dog <theblindguidedog@Horaces-iMac.local>
2020-07-31 17:03:12 +1000

    After reinstall OP

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    }
+}
 
 DATABASE_URI = 'postgres://ryatgtibmfhcsu:098367d7ca2fd2d079775e5e3268a969774dea002e4ec5f2bd6d3c29ad747d7e@ec2-35-175-155-248.compute-1.amazonaws.com:5432/dc4anhef5vd2a1'
 
